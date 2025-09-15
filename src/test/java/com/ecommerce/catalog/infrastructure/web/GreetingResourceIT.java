9400dd7aa80e4e76749dd35e6f53cea8520c4a77
Juan Pablo Bedoya Benavides <juanp.bedoyab+davicode@proveedores.davivienda.com>
2025-09-14 19:31:11 -0500

    Init project

---
+++ src/test/java/com/ecommerce/catalog/infrastructure/web/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.ecommerce.catalog.infrastructure.web;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
